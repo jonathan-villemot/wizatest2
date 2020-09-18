694f67efd8127aa9cd0e31dca5533c9c0f190882
Jonathan Villemot <jonathan@getawriggleon.com>
2020-09-18 17:27:10 +0100

    fix tests

--- bootstrapValidator.test.js
+++ bootstrapValidator.test.js
@@ -3,18 +3,22 @@ const bsv = require('./bootstrapValidator');
 describe('boostrapValidator', () => {
 
     test('mainString is mandatory', () => {
-        expect(bsv.validate(undefined, '2')).toThrow('Missing Main string as 1st argument');
+        expect(() => {bsv.validate(undefined, '2')}).toThrow('Missing Main string as 1st argument');
     });
 
     test('N is mandatory', () => {
-        expect(bsv.validate('12312342214', undefined)).toThrow('Missing Main string as 1st argument');
+        expect(() => {bsv.validate('12312342214', undefined)}).toThrow('Missing Parameter N for substring length as 2nd argument');
     });
 
     test('accepts only numbers in mainString and N parameters', () => {
-        expect(bsv.validate('1231N2342214', '2')).toThrow('Main string must be a string with only numbers');
+        expect(() => {bsv.validate('1231N2342214', '2')}).toThrow('Main string must be a string with only numbers');
     });
 
     test('accepts only numbers in mainString and N parameters', () => {
-        expect(bsv.validate('12312342214', '2N')).toThrow('N parameter must be a string with only numbers');
+        expect(() => {bsv.validate('12312342214', '2N')}).toThrow('N parameter must be a string with only numbers');
+    });
+
+    test('accepts only numbers in mainString and N parameters', () => {
+        expect(() => {bsv.validate('12312342214', '2')}).not.toThrow();
     });
 });
